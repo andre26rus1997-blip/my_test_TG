4426461013098cc5098503fb27329873c174e88e
andre26rus1997-blip <andre26rus1997@gmail.com>
2025-09-28 16:33:21 +0300

    test1

--- TGbot.py
+++ TGbot.py
@@ -1,51 +1,13 @@
 import telebot
 from telebot import types
+from keyboards import main_menu,my_help,info
+
 import requests
+
 
 bot = telebot.TeleBot("8429683137:AAEhGBEH0AAIAxkCojS3aNDbfqMcGwmdHpA")
 API = "9d25c51fa1e0dfe91110828f36f71afb"
 
-# Главное меню
-
-
-def main_menu():
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    btn1 = types.InlineKeyboardButton("🤝Помощь✌️", callback_data="help")
-    btn2 = types.InlineKeyboardButton("📝Информация📌", callback_data="info")
-    btn3 = types.InlineKeyboardButton(
-        "🥳Посмейся немного🤣", callback_data="jok")
-    btn4 = types.InlineKeyboardButton("⛅Погода🌧️", callback_data="weather")
-    btn5 = types.InlineKeyboardButton("❌Удалить🗑️", callback_data="delete")
-    markup.add(btn1, btn2, btn3, btn4, btn5)
-    return markup
-
-
-# keyboard for help
-def my_help():  #
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    btn1 = types.InlineKeyboardButton(
-        "😍Как пользоваться мной😍", callback_data="How_used"
-    )
-    btn2 = types.InlineKeyboardButton(
-        "❤️Что я могу❤️", callback_data="ability")
-    btn3 = types.InlineKeyboardButton(
-        "😭Напиши мне свою проблему😢", callback_data="trouble"
-    )
-    btn4 = types.InlineKeyboardButton("⬅️ 😏Назад😏", callback_data="back")
-    markup.add(btn1, btn2, btn3, btn4)
-    return markup
-
-
-# keyboard for info
-def info():
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    btn1 = types.InlineKeyboardButton(
-        "O проекте", callback_data="about_project")
-    btn2 = types.InlineKeyboardButton("Контакты", callback_data="contacts")
-    btn_back = types.InlineKeyboardButton(
-        "️⬅️Назад⬅️", callback_data="back_main")
-    markup.add(btn1, btn2, btn_back)
-    return markup
 
 
 simple_responses = {
